95b97c7eeed4230673aeda9e532c7e0b827a2c14
Faheem Riaz <faheem@digitify.com>
2021-08-04 09:51:46 +0500

    -Code issue resolved

--- app/src/main/java/com/digitify/testyappakistan/onboarding/MainViewModel.kt
+++ app/src/main/java/com/digitify/testyappakistan/onboarding/MainViewModel.kt
@@ -1,8 +1,9 @@
 package com.digitify.testyappakistan.onboarding
 
 import com.yap.core.base.BaseViewModel
+import com.yap.core.base.SingleClickEvent
 
 class MainViewModel : BaseViewModel<IMain.State>(), IMain.ViewModel {
-    override val state: IMain.State = MainState()
-
+    override val viewState: IMain.State = MainState()
+    override val clickEvent: SingleClickEvent  = SingleClickEvent()
 }
